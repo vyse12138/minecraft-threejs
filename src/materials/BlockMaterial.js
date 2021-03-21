e857dcce74e7810cd7b06fdb381544637b6dabae
Vyse12138 <zhuyulei12333@gmail.com>
2021-03-21 21:50:54 +1100

    update .gitignore

--- src/materials/BlockMaterial.js
+++ src/materials/BlockMaterial.js
@@ -1,13 +1,13 @@
 import * as THREE from "three";
-const stone = new THREE.TextureLoader().load("src/assets/blocks/stone.png");
+const stone = new THREE.TextureLoader().load("./src/assets/blocks/stone.png");
 stone.magFilter = THREE.NearestFilter;
 const stoneMaterial = new THREE.MeshBasicMaterial({ map: stone });
 
-const coal = new THREE.TextureLoader().load("src/assets/blocks/coal_ore.png");
+const coal = new THREE.TextureLoader().load("./src/assets/blocks/coal_ore.png");
 coal.magFilter = THREE.NearestFilter;
 const coalMaterial = new THREE.MeshBasicMaterial({ map: coal });
 
-const iron = new THREE.TextureLoader().load("src/assets/blocks/iron_ore.png");
+const iron = new THREE.TextureLoader().load("./src/assets/blocks/iron_ore.png");
 iron.magFilter = THREE.NearestFilter;
 const ironMaterial = new THREE.MeshBasicMaterial({ map: iron });
 
