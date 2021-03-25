2dd8d3ae877a5fb9459dcac0214ff7b10bc8f509
Vyse12138 <zhuyulei12333@gmail.com>
2021-03-25 17:51:33 +1100

    Update TerrainGenerator.js

--- src/terrains/TerrainGenerator.js
+++ src/terrains/TerrainGenerator.js
@@ -12,15 +12,16 @@ export default class TerrainGenerator {
   build() {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     let mesh = new THREE.InstancedMesh(geometry, this.grassMaterial, 35000);
+    mesh.receiveShadow = true;
 
     let i = 0;
     const matrix = new THREE.Matrix4();
     const color = new THREE.Color();
 
-    for (let x = 0; x < 150; x++) {
+    for (let x = 0; x < 128; x++) {
       for (let y = 0; y < 2; y++) {
-        for (let z = 0; z < 150; z++) {
-          let v = Math.round(this.noise.noise2D(x / 128, z / 128) * 12);
+        for (let z = 0; z < 128; z++) {
+          let v = Math.round(this.noise.noise2D(x / 160, z / 160) * 15);
           matrix.setPosition(x, y + v, z);
           mesh.setMatrixAt(i, matrix);
           mesh.setColorAt(i++, color);
