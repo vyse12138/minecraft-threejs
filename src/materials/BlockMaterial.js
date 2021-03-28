c263a0ffd2cf067bd198542588d8f6f5d1beef8b
Vyse12138 <zhuyulei12333@gmail.com>
2021-03-28 14:37:18 +1100

    tree generator

--- src/materials/BlockMaterial.js
+++ src/materials/BlockMaterial.js
@@ -1,48 +1,86 @@
 import * as THREE from "three";
-import stoneTexture from "../assets/blocks/stone.png";
-import coalTexture from "../assets/blocks/coal_ore.png";
-import ironTexture from "../assets/blocks/iron_ore.png";
+import stone from "../assets/blocks/stone.png";
+import coal_ore from "../assets/blocks/coal_ore.png";
+import iron_ore from "../assets/blocks/iron_ore.png";
 import grass_side from "../assets/blocks/grass_side.png";
 import grass_top_green from "../assets/blocks/grass_top_green.png";
 import dirt from "../assets/blocks/dirt.png";
-
+import oak_log from "../assets/blocks/oak_log.png";
+import oak_log_top from "../assets/blocks/oak_log_top.png";
+import oak_leaves from "../assets/blocks/oak_leaves.png";
 
 let loader = new THREE.TextureLoader();
 
-const grassTop = loader.load(grass_top_green);
-grassTop.magFilter = THREE.NearestFilter;
+const grassTopMaterial = loader.load(grass_top_green);
+const grassMaterial = loader.load(grass_side);
+const treeMaterial = loader.load(oak_log);
+const treeTopMaterial = loader.load(oak_log_top);
+const dirtMaterial = loader.load(dirt);
+const stoneMaterial = loader.load(stone);
+const coalMaterial = loader.load(coal_ore);
+const ironMaterial = loader.load(iron_ore);
+const leafMaterial = loader.load(oak_leaves);
 
-const grassSide = loader.load(grass_side);
-grassSide.magFilter = THREE.NearestFilter;
-
-const dirtT = loader.load(dirt);
-dirtT.magFilter = THREE.NearestFilter;
-
-const stone = loader.load(stoneTexture);
-stone.magFilter = THREE.NearestFilter;
-
-const coal = loader.load(coalTexture);
-coal.magFilter = THREE.NearestFilter;
-
-const iron = loader.load(ironTexture);
-iron.magFilter = THREE.NearestFilter;
-
-
-
+grassTopMaterial.magFilter = THREE.NearestFilter;
+grassMaterial.magFilter = THREE.NearestFilter;
+treeMaterial.magFilter = THREE.NearestFilter;
+treeTopMaterial.magFilter = THREE.NearestFilter;
+dirtMaterial.magFilter = THREE.NearestFilter;
+stoneMaterial.magFilter = THREE.NearestFilter;
+coalMaterial.magFilter = THREE.NearestFilter;
+ironMaterial.magFilter = THREE.NearestFilter;
+leafMaterial.magFilter = THREE.NearestFilter;
 
 export default function BlockMaterial(name) {
   let material;
-  if (name === "grass") {
-    material = [
-      new THREE.MeshStandardMaterial( { map: grassSide }),
-      new THREE.MeshStandardMaterial( { map: grassSide }),
-        new THREE.MeshStandardMaterial( { map: grassTop }),
-        new THREE.MeshStandardMaterial( { map: dirtT }),
-        new THREE.MeshStandardMaterial( { map: grassSide }),
-        new THREE.MeshStandardMaterial( { map: grassSide })
-    ];
-  } else {
-    material = new THREE.MeshStandardMaterial({ map: dirtT });
+  switch (name) {
+    case "grass":
+      material = [
+        new THREE.MeshStandardMaterial({ map: grassMaterial }),
+        new THREE.MeshStandardMaterial({ map: grassMaterial }),
+        new THREE.MeshStandardMaterial({ map: grassTopMaterial }),
+        new THREE.MeshStandardMaterial({ map: dirtMaterial }),
+        new THREE.MeshStandardMaterial({ map: grassMaterial }),
+        new THREE.MeshStandardMaterial({ map: grassMaterial })
+      ];
+      break;
+    case "dirt":
+      material = new THREE.MeshStandardMaterial({ map: dirtMaterial });
+      break;
+    case "tree":
+      material = [
+        new THREE.MeshStandardMaterial({ map: treeMaterial }),
+        new THREE.MeshStandardMaterial({ map: treeMaterial }),
+        new THREE.MeshStandardMaterial({ map: treeTopMaterial }),
+        new THREE.MeshStandardMaterial({ map: treeTopMaterial }),
+        new THREE.MeshStandardMaterial({ map: treeMaterial }),
+        new THREE.MeshStandardMaterial({ map: treeMaterial })
+      ];
+      break;
+    case "leaf":
+      material = new THREE.MeshStandardMaterial({ map: leafMaterial });
+      material.color = new THREE.Color(0,1,0);
+      material.transparent = true;
+      break;
+    case "stone":
+      material = new THREE.MeshStandardMaterial({ map: stoneMaterial });
+      break;
+    case "iron":
+      material = new THREE.MeshStandardMaterial({ map: ironMaterial });
+      break;
+    case "coal":
+      material = new THREE.MeshStandardMaterial({ map: coalMaterial });
+      break;
+    default:
+      material = [
+        new THREE.MeshStandardMaterial({ map: grassMaterial }),
+        new THREE.MeshStandardMaterial({ map: grassMaterial }),
+        new THREE.MeshStandardMaterial({ map: grassTopMaterial }),
+        new THREE.MeshStandardMaterial({ map: dirtMaterial }),
+        new THREE.MeshStandardMaterial({ map: grassMaterial }),
+        new THREE.MeshStandardMaterial({ map: grassMaterial })
+      ];
+      break;
   }
   return material;
 }
--- src/terrains/TerrainGenerator.js
+++ src/terrains/TerrainGenerator.js
@@ -7,30 +7,90 @@ export default class TerrainGenerator {
     this.scene = scene;
     this.grassMaterial = new BlockMaterial("grass");
     this.dirtMaterial = new BlockMaterial("dirt");
+    this.treeMaterial = new BlockMaterial("tree");
+    this.leafMaterial = new BlockMaterial("leaf");
+    this.leafMap = new Set();
     this.noise = new simplex.SimplexNoise();
   }
   build() {
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    let mesh = new THREE.InstancedMesh(geometry, this.grassMaterial, 35000);
-    // mesh.receiveShadow = true;
-    let i = 0;
+    const grassGeometry = new THREE.BoxGeometry(1, 1, 1);
+    const grass = new THREE.InstancedMesh(
+      grassGeometry,
+      this.grassMaterial,
+      20000
+    );
+    const treeGeometry = new THREE.BoxGeometry(1, 1, 1);
+    const tree = new THREE.InstancedMesh(treeGeometry, this.treeMaterial, 3000);
+    const leafGeometry = new THREE.BoxGeometry(1, 1, 1);
+    const leaf = new THREE.InstancedMesh(leafGeometry, this.leafMaterial, 8000);
+    let grassCount = 0,
+      treeCount = 0,
+      leafCount = 0;
     const matrix = new THREE.Matrix4();
     const color = new THREE.Color();
 
     for (let x = 0; x < 128; x++) {
-      for (let y = 0; y < 2; y++) {
+      for (let y = 0; y < 1; y++) {
         for (let z = 0; z < 128; z++) {
-          let v = Math.round(this.noise.noise2D(x / 160, z / 160) * 15);
-          matrix.setPosition(x, y + v, z);
-          mesh.setMatrixAt(i, matrix);
-          mesh.setColorAt(i++, color);
+          let noise = Math.round(this.noise.noise2D(x / 160, z / 160) * 15);
+          matrix.setPosition(x, y + noise, z);
+          grass.setMatrixAt(grassCount, matrix);
+          grass.setColorAt(grassCount++, color);
+          if (Math.random() > 0.999) {
+            let height = 5;
+            while (height--) {
+
+              matrix.setPosition(x, y + noise + 1 + height, z);
+              this.leafMap.add(matrix.elements.join());
+
+              tree.setMatrixAt(treeCount, matrix);
+              tree.setColorAt(treeCount++, color);
+            }
+            height = 5;
+            for (let x2 = 0; x2 < 8; x2++) {
+              for (let y2 = 0; y2 < 3; y2++) {
+                for (let z2 = 0; z2 < 8; z2++) {
+                  let noiseX = Math.round(
+                    this.noise.noise3D(y2, z2, Math.random()) * 3
+                  );
+                  let noiseY = Math.round(
+                    this.noise.noise3D(x2, z2, Math.random()) * 2
+                  );
+                  let noiseZ = Math.round(
+                    this.noise.noise3D(x2, y2, Math.random()) * 3
+                  );
+
+                  matrix.setPosition(
+                    x + noiseX,
+                    y + noise + noiseY + height,
+                    z + noiseZ
+                  );
+                  if (!this.hasBlock(matrix)) {
+                    leaf.setMatrixAt(leafCount, matrix);
+                    leaf.setColorAt(leafCount++, color);
+                  }
+                }
+              }
+            }
+          }
         }
       }
     }
-    this.scene.add(mesh);
-    mesh.geometry.computeBoundingBox();
-let bb = mesh.geometry.boundingBox
-console.log(bb);
-    return [mesh];
+    this.scene.add(grass);
+    this.scene.add(tree);
+    this.scene.add(leaf);
+    console.log(grass);
+    console.log(leaf);
+
+    return [grass, tree, leaf];
+  }
+
+  hasBlock(matrix) {
+    if (this.leafMap.has(matrix.elements.join())) {
+      return true;
+    } else {
+      this.leafMap.add(matrix.elements.join());
+      return false;
+    }
   }
 }
