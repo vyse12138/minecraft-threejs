878d0a63157249090f62c75035e9417cd1fb50e2
Vyse12138 <zhuyulei12333@gmail.com>
2021-03-28 15:27:34 +1100

    light update

--- src/init/Scene.js
+++ src/init/Scene.js
@@ -5,28 +5,22 @@ export default function initScene() {
   scene.name = "SCENE";
   scene.fog = new THREE.FogExp2(backgroundColor, 0.003);
 
-  const light1 = new THREE.PointLight( 0xffffff, 1 );
-  light1.position.set( 500, 500, 500 );
-  // light1.castShadow = true; 
-  scene.add( light1 );
-  const light2 = new THREE.PointLight( 0xffffff, 0.33 );
-  light2.position.set( -500, 500, -500 );
-  scene.add( light2 );
-  const light3 = new THREE.PointLight( 0xffffff, 0.1 );
-  light3.position.set( 500, -500, 500 );
-  scene.add( light3 );
-  const light4 = new THREE.PointLight( 0xffffff, 0.1 );
-  light4.position.set( -500, -500, -500 );
-  scene.add( light4 );
+  const light1 = new THREE.PointLight(0xffffff, 0.5);
+  light1.position.set(500, 500, 500);
+  // light1.castShadow = true;
+  scene.add(light1);
 
-  const geometry = new THREE.SphereGeometry( 5, 32, 32 );
-  const material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-  const sun = new THREE.Mesh( geometry, material );
+  const light = new THREE.AmbientLight(0x404040); // soft white light
+  scene.add(light);
+
+  const geometry = new THREE.SphereGeometry(5, 32, 32);
+  const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  const sun = new THREE.Mesh(geometry, material);
   sun.position.y = 100;
   sun.position.x = 200;
   sun.position.z = 150;
 
-  scene.add( sun );
+  scene.add(sun);
 
   scene.background = new THREE.Color(backgroundColor);
 
