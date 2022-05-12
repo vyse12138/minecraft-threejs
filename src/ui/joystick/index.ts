557159103b9ce58efae733bad6fdf5998d2dba87
Yulei Zhu <zhuyulei12333@gmail.com>
2022-05-12 18:51:20 +1000

    refactor: joystick code clean up

--- src/ui/joystick/index.ts
+++ src/ui/joystick/index.ts
@@ -1,81 +1,73 @@
 import Control from '../../control'
 import { Mode } from '../../player'
-import { initJoystick } from './core'
+import { htmlToDom } from '../../utils'
+import UI from './joystick.html?raw'
+
+enum ActionKey {
+  FRONT = 'front',
+  LEFT = 'left',
+  RIGHT = 'right',
+  BACK = 'back',
+  MODE = 'mode',
+  JUMP = 'jump',
+  UP = 'up',
+  DOWN = 'down'
+}
 
 export default class Joystick {
   constructor(control: Control) {
     this.control = control
   }
 
+  control: Control
+
+  // emit keyboard event
   private emitKeyboardEvent = (key: string) => {
     return {
       key
     } as KeyboardEvent
   }
 
-  control: Control
+  // init joystick button
+  private initButton = ({
+    actionKey,
+    key
+  }: {
+    actionKey: ActionKey
+    key: string
+  }) => {
+    const button = document.querySelector(
+      `#action-${actionKey}`
+    ) as HTMLButtonElement
+    button.addEventListener('pointerenter', () => {
+      this.control.setMovementHandler(this.emitKeyboardEvent(key))
+    })
+    button.addEventListener('pointerleave', () => {
+      this.control.resetMovementHandler(this.emitKeyboardEvent(key))
+    })
+
+    // extra config for mode switch button
+    if (actionKey === ActionKey.MODE && key === 'q') {
+      this.initButton({ actionKey: ActionKey.MODE, key: ' ' })
+      button.addEventListener('pointerenter', () => {
+        if (this.control.player.mode === Mode.flying) {
+          document.querySelector('#action-down')?.classList.remove('hidden')
+        } else {
+          document.querySelector('#action-down')?.classList.add('hidden')
+        }
+      })
+    }
+  }
+
   init = () => {
-    const joystick = initJoystick()
+    htmlToDom(UI)
 
-    joystick.GO.onPress(() => {
-      this.control.setMovementHandler(this.emitKeyboardEvent('w'))
-    }).onLoosen(() => {
-      this.control.resetMovementHandler(this.emitKeyboardEvent('w'))
-    })
-
-    joystick.LEFT.onPress(() => {
-      this.control.setMovementHandler(this.emitKeyboardEvent('a'))
-    }).onLoosen(() => {
-      this.control.resetMovementHandler(this.emitKeyboardEvent('a'))
-    })
-
-    joystick.RIGHT.onPress(() => {
-      this.control.setMovementHandler(this.emitKeyboardEvent('d'))
-    }).onLoosen(() => {
-      this.control.resetMovementHandler(this.emitKeyboardEvent('d'))
-    })
-
-    joystick.BACK.onPress(() => {
-      this.control.setMovementHandler(this.emitKeyboardEvent('s'))
-    }).onLoosen(() => {
-      this.control.resetMovementHandler(this.emitKeyboardEvent('s'))
-    })
-
-    joystick.JUMP.onPress(() => {
-      this.control.setMovementHandler(this.emitKeyboardEvent(' '))
-    }).onLoosen(() => {
-      this.control.resetMovementHandler(this.emitKeyboardEvent(' '))
-    })
-
-    joystick.CENTER.onPress(() => {
-      this.control.setMovementHandler(this.emitKeyboardEvent('q'))
-      this.control.setMovementHandler(this.emitKeyboardEvent(' '))
-
-      if (this.control.player.mode === Mode.flying) {
-        joystick.UP.toggleVisible(true)
-        joystick.DOWN.toggleVisible(true)
-      } else {
-        joystick.UP.toggleVisible(false)
-        joystick.DOWN.toggleVisible(false)
-      }
-    }).onLoosen(() => {
-      this.control.resetMovementHandler(this.emitKeyboardEvent(' '))
-    })
-
-    joystick.UP.onPress(() => {
-      this.control.setMovementHandler(this.emitKeyboardEvent(' '))
-    })
-      .onLoosen(() => {
-        this.control.resetMovementHandler(this.emitKeyboardEvent(' '))
-      })
-      .toggleVisible(false)
-
-    joystick.DOWN.onPress(() => {
-      this.control.setMovementHandler(this.emitKeyboardEvent('Shift'))
-    })
-      .onLoosen(() => {
-        this.control.resetMovementHandler(this.emitKeyboardEvent('Shift'))
-      })
-      .toggleVisible(false)
+    this.initButton({ actionKey: ActionKey.FRONT, key: 'w' })
+    this.initButton({ actionKey: ActionKey.LEFT, key: 'a' })
+    this.initButton({ actionKey: ActionKey.RIGHT, key: 'd' })
+    this.initButton({ actionKey: ActionKey.BACK, key: 's' })
+    this.initButton({ actionKey: ActionKey.MODE, key: 'q' })
+    this.initButton({ actionKey: ActionKey.UP, key: ' ' })
+    this.initButton({ actionKey: ActionKey.DOWN, key: 'Shift' })
   }
 }
